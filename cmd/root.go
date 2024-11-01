18cf27a7f16cec480127dce4591dbb652e88e648
felix <felixpowerbusiness@gmail.com>
2024-11-01 22:47:06 +0000

    Added viper for config

--- cmd/root.go
+++ cmd/root.go
@@ -4,12 +4,15 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	// "fmt"
+	"fmt"
 	"launcher/tui/list"
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -39,6 +42,7 @@ func Execute() {
 }
 
 func init() {
+	cobra.OnInitialize(initConfig)
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
@@ -50,4 +54,29 @@ func init() {
 	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 	// rootCmd.Flags().StringP("path", "p", "", "optional path")
 	rootCmd.PersistentFlags().StringP("path", "p", "", "optional path")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		fmt.Println("Home directory:", home)
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".launcher" (without extension).
+		viper.AddConfigPath(home + "/.config")
+		viper.SetConfigType("json")
+		viper.SetConfigName("launcher.json")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
 }
